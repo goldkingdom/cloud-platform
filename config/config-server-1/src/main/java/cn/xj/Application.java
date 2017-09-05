c7ad1505cbfc5ae88393c578757cac91dbdea30a
goldkingdom <274852853@qq.com>
2017-09-05 18:02:24 +0800

    amqp

--- config/config-server-1/src/main/java/cn/xj/Application.java
+++ config/config-server-1/src/main/java/cn/xj/Application.java
@@ -1,18 +1,17 @@
 package cn.xj;
 
-import cn.xj.model.UserInfo;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableConfigurationProperties(UserInfo.class)
+@EnableConfigServer
 public class Application {
 
     public static void main(String[] args) {
-        new SpringApplicationBuilder(Application.class).web(true).run(args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
